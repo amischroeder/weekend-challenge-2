44a7f14ff6804477c8a4754100614fcdd1a7ae70
Ami Schroeder <arschroeder22@gmail.com>
2017-08-05 09:40:10 -0500

    sendStatus

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var port = 5000;
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
+app.post('/calculator', function(req,res){
+    console.log('Message post was a hit!');
+    res.sendStatus(201);
+});
+
 app.listen(port, function(){
     console.log('Running on port', port);
 });
--- public/client.js
+++ public/client.js
@@ -3,5 +3,17 @@ $(document).ready(function (){
         console.log('addButton clicked!');
         var firstNumber = $('#numberOne').val();
         var secondNumber = $('#numberTwo').val();
+        var inputObject = {
+            firstInput: firstNumber,
+            secondInput: secondNumber
+        }
+        $.ajax({
+            method: 'POST',
+            url: '/calculator',
+            data: inputObject,
+            success: function (response) {
+                console.log(response);
+            }
+        })
     });
 });

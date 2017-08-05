9ddaf7b03ef5d1a829f0e84cb1c76a27164d226b
Ami Schroeder <arschroeder22@gmail.com>
2017-08-05 12:56:59 -0500

    get first 3 buttons to function

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = 5000;
 
+
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.post('/calculator', function(req,res){
+    console.log(req.body);
     console.log('Message post was a hit!');
-    res.sendStatus(201);
+    res.send(req.body);
 });
+
+app.get('/calculator', function(req,res){
+    res.send(req.body);
+})
 
 app.listen(port, function(){
     console.log('Running on port', port);
--- public/client.js
+++ public/client.js
@@ -1,11 +1,16 @@
-$(document).ready(function (){
-    $('#addButton').on('click', function() {
+$(document).ready(function () {
+    $('#addButton').on('click', function () {
         console.log('addButton clicked!');
         var firstNumber = $('#numberOne').val();
         var secondNumber = $('#numberTwo').val();
         var inputObject = {
             firstInput: firstNumber,
-            secondInput: secondNumber
+            secondInput: secondNumber,
+            type: "Add"
+        }
+        function addNumbers() {
+            var sum = parseInt(inputObject.firstInput) + parseInt(inputObject.secondInput);
+            $('body').append('<div>' + sum + '</div>')
         }
         $.ajax({
             method: 'POST',
@@ -13,7 +18,86 @@ $(document).ready(function (){
             data: inputObject,
             success: function (response) {
                 console.log(response);
+                addNumbers();
             }
         })
     });
+    function getNumbers() {
+        $.ajax({
+            method: 'GET',
+            url: '/calculator',
+            success: function (response) {
+                console.log(response);
+                addNumbers(response);
+            }
+        })
+    }
+    $('#subtractButton').on('click', function () {
+        console.log('subtractButton clicked!');
+        var firstNumber = $('#numberOne').val();
+        var secondNumber = $('#numberTwo').val();
+        var inputObject = {
+            firstInput: firstNumber,
+            secondInput: secondNumber,
+            type: "Subtract"
+        }
+        function subtractNumbers() {
+            var sum = parseInt(inputObject.firstInput) - parseInt(inputObject.secondInput);
+            $('body').append('<div>' + sum + '</div>')
+        }
+        $.ajax({
+            method: 'POST',
+            url: '/calculator',
+            data: inputObject,
+            success: function (response) {
+                console.log(response);
+                subtractNumbers();
+            }
+        })
+
+        function getNumbers() {
+            $.ajax({
+                method: 'GET',
+                url: '/calculator',
+                success: function (response) {
+                    console.log(response);
+                    subtractNumbers(response);
+                }
+            })
+        }
+    })
+    $('#multiplyButton').on('click', function () {
+        console.log('multiplyButton clicked!');
+        var firstNumber = $('#numberOne').val();
+        var secondNumber = $('#numberTwo').val();
+        var inputObject = {
+            firstInput: firstNumber,
+            secondInput: secondNumber,
+            type: "Multiply"
+        }
+        function multiplyNumbers() {
+            var sum = parseInt(inputObject.firstInput) * parseInt(inputObject.secondInput);
+            $('body').append('<div>' + sum + '</div>')
+        }
+        $.ajax({
+            method: 'POST',
+            url: '/calculator',
+            data: inputObject,
+            success: function (response) {
+                console.log(response);
+                multiplyNumbers();
+            }
+        })
+
+        function getNumbers() {
+            $.ajax({
+                method: 'GET',
+                url: '/calculator',
+                success: function (response) {
+                    console.log(response);
+                    multiplyNumbers(response);
+                }
+            })
+        }
+    })
 });

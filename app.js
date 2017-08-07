99dd30c9de9b8c1b5c8a9cc8ac6a69d4e629e3d6
Ami Schroeder <arschroeder22@gmail.com>
2017-08-06 20:43:34 -0500

    everything on server but calculations off

--- app.js
+++ app.js
@@ -3,19 +3,33 @@ var bodyParser = require('body-parser');
 
 var app = express();
 var port = 5000;
-
+var mathResult = [];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 
+function calculator(x,y,z){
+    if (z === 'add'){
+        mathResult = [parseInt(x) + parseInt(y)]
+    } else if (z === 'subtract'){
+        mathResult = [parseInt(x) - parseInt(y)]
+    } else if (z === 'multiply'){
+        mathResult = [parseInt(x) * parseInt(y)]
+    } else if (z === 'divide')
+    {
+        mathResult = [parseInt(x) / parseInt(y)]
+    }
+};
+
 app.post('/calculator', function(req,res){
-    console.log(req.body);
-    console.log('Message post was a hit!');
-    res.send(req.body);
+    console.log('message post was hit!');
+    calculator(req.body.firstInput, req.body.secondInput, req.body.type);
+    console.log(mathResult);
+    res.sendStatus(201);
 });
 
 app.get('/calculator', function(req,res){
-    res.send(req.body);
+    res.send(mathResult);
 })
 
 app.listen(port, function(){
--- public/client.js
+++ public/client.js
@@ -1,95 +1,75 @@
 $(document).ready(function () {
-    //  function getNumbers() {
-    //     $.ajax({
-    //         method: 'GET',
-    //         url: '/calculator',
-    //         success: function (response) {
-    //             console.log(response);
-    //             addNumbers (response);
-    //             subtractNumbers(response);
-    //             multiplyNumbers (response);
-    //             divideNumbers (response);
-    //         }
-    //     })
-    // }
     $('.mathButtons').on('click', function () {
+        getNumbers();
         console.log('Math button clicked');
         var firstNumber = $('#numberOne').val();
         var secondNumber = $('#numberTwo').val(); 
         var mathType = $(this).attr('id');
+        console.log(mathType);
+
         var inputObject = {
             firstInput: firstNumber,
             secondInput: secondNumber,
             type: mathType
         };
+        console.log(inputObject);
 
-    })
-    $('#addButton').on('click', function () {
-        function addNumbers() {
-            var sum = parseInt(inputObject.firstInput) + parseInt(inputObject.secondInput);
-            $('#answerSpace').append('<div>' + sum + '</div>')
-        }
-         $.ajax({
+        $.ajax({
              method: 'POST',
              url: '/calculator',
              data: inputObject,
              success: function (response) {
                  console.log(response);
-                 addNumbers();
+                 getNumbers();
              }
          })
-    });
-    $('#subtractButton').on('click', function () {
-        function subtractNumbers() {
-            var sum = parseInt(inputObject.firstInput) - parseInt(inputObject.secondInput);
-            $('#answerSpace').append('<div>' + sum + '</div>')
-        }
-         $.ajax({
-             method: 'POST',
-             url: '/calculator',
-             data: inputObject,
-             success: function (response) {
-                 console.log(response);
-                 subtractNumbers();
-             }
-         })
-    })
-    $('#multiplyButton').on('click', function () {
-        console.log('multiplyButton clicked!');
-        function multiplyNumbers() {
-            var sum = parseInt(inputObject.firstInput) * parseInt(inputObject.secondInput);
-            $('#answerSpace').append('<div>' + sum + '</div>')
-        }
-         $.ajax({
-             method: 'POST',
-             url: '/calculator',
-             data: inputObject,
-             success: function (response) {
-                 console.log(response);
-                 multiplyNumbers();
-             }
-         })
-    })
-    $('#divideButton').on('click', function () {
-        console.log('divideButton clicked!');
-        function divideNumbers() {
-            var sum = parseInt(inputObject.firstInput) / parseInt(inputObject.secondInput);
-            $('#answerSpace').append('<div>' + sum + '</div>')
-        }
-         $.ajax({
-             method: 'POST',
-             url: '/calculator',
-             data: inputObject,
-             success: function (response) {
-                 console.log(response);
-                 divideNumbers();
-             }
-         })
-    })
+    }) //end click math button
+
+      function getNumbers() {
+        $.ajax({
+            method: 'GET',
+            url: '/calculator',
+            success: function (response) {
+                console.log(response);
+                getResult(response);
+            }
+        })
+    }
+
+    function getResult(response){
+        $('#answerSpace').append(response);
+    }
     $('#clearButton').on('click', function () {
         console.log('clearButton clicked!');
         $('#numberOne').val('');
         $('#numberTwo').val('');
         $('#answerSpace').empty();
     })
+
+    // $('#addButton').on('click', function () {
+    //     function addNumbers() {
+    //         var sum = parseInt(inputObject.firstInput) + parseInt(inputObject.secondInput);
+    //         $('#answerSpace').append('<div>' + sum + '</div>')
+    //     }
+    // });
+    // $('#subtractButton').on('click', function () {
+    //     function subtractNumbers() {
+    //         var sum = parseInt(inputObject.firstInput) - parseInt(inputObject.secondInput);
+    //         $('#answerSpace').append('<div>' + sum + '</div>')
+    //     }
+    // })
+    // $('#multiplyButton').on('click', function () {
+    //     console.log('multiplyButton clicked!');
+    //     function multiplyNumbers() {
+    //         var sum = parseInt(inputObject.firstInput) * parseInt(inputObject.secondInput);
+    //         $('#answerSpace').append('<div>' + sum + '</div>')
+    //     }
+    // })
+    // $('#divideButton').on('click', function () {
+    //     console.log('divideButton clicked!');
+    //     function divideNumbers() {
+    //         var sum = parseInt(inputObject.firstInput) / parseInt(inputObject.secondInput);
+    //         $('#answerSpace').append('<div>' + sum + '</div>')
+    //     }
+    // })
 });
